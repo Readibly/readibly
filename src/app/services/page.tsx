e006ed3e078cf57a80e53ccc2599f0e5f876dbbf
MRizainF <icon.firdaus@gmail.com>
2025-03-26 09:56:48 +0700

    feat: add services and about page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import type React from 'react'
+
+function Services() {
+  return (
+    <div>
+        <h1>
+            On Dev
+        </h1>
+    </div>
+  )
+}
+
+export default Services
