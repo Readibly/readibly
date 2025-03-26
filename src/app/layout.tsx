806d98b1305c3bef2de207ab3c30807943a813cb
MRizainF <icon.firdaus@gmail.com>
2025-03-26 11:41:32 +0700

    feat: update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,8 @@ import type React from "react"
 import Footer from "@/components/footer"
 import Navbar from "@/components/navbar"
 import {ThemeProvider} from "next-themes"
-import {Inter} from "next/font/google"
 import "./globals.css"
 
-const inter = Inter({subsets: ["latin"]});
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -14,7 +12,7 @@ interface RootLayoutProps {
 function RootLayout({ children }: RootLayoutProps) {
   return(
     <html lang="id" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <div className="flex min-h-screen flex-col">
             <Navbar></Navbar>
