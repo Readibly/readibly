b03f84cd20df5538528f51a97e0fded67c2519a9
MRizainF <icon.firdaus@gmail.com>
2025-04-02 17:15:51 +0700

    style: enhance Home component layout and visuals with updated styles, improved button designs, and refined text elements

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,72 +5,79 @@ import { ArrowRight, Eye, Headphones, BookOpen } from "lucide-react";
 import AnimatedText from "@/components/AnimatedText";
 import FeatureCard from "@/components/FeatureCard";
 
+
 export default function Home() {
   return (
     // hero section
-    <div className="flex flex-col">
-      <section className="w-full min-h-screen py-12 md:py-24 lg:py-18 bg-[#FDFCF4]">
+    <div className="flex flex-col mb-4">
+      <section className="w-full min-h-screen py-4 bg-[#FDFCF4]">
         <div className="container px-4 md:px-6">
           <div className="grid gap-6 lg:grid-cols-2 lg:gap-12 items-center">
             <div className="space-y-4 pl-26">
-              <h1 className="text-9xl font-medium sm:text-4xl md:text-5xl lg:text-7xl text-gray-800">
-                <AnimatedText className="font-bold text-white"></AnimatedText> Easily
+              <h1 className="text-9xl font-semibold sm:text-4xl md:text-5xl lg:text-7xl text-[#1e1e1e]">
+                <AnimatedText className="font-bold text-[#2e31ce] inline-block"></AnimatedText> Easily
               </h1>
               <p className="text-lg font-normal med:text-xl text-gray-800">
-                Readibly helps people with dyslexia to have the same and equal chance to gain information by reading using eye-tracking and text-to-speech technology. Try our service for free right now.
+                Readibly helps people with dyslexia to have the same and equal chance to gain information by reading using eye-tracking and text-to-speech technology. Try Readibly for free right now.
               </p>
-              <div className="flex flex-col sm:flex-row gap-4 py-4">
+              <div className="flex flex-col sm:flex-row gap-4 py-2">
                 <Link href="/reader">
-                  <Button className="bg-[#7678ed] text-base font-medium hover:bg-[#9294f1] px-6 py-4 h-auto">
+                  <Button className="bg-gradient-to-br from-[#2e31ce] via-[#772abe] to-[#9f53e6] text-base font-medium shadow-[0px_0px_10px_rgba(0,0,0,0.2)] hover:bg-gradient-to-br hover:from-[#373ad3] hover:via-[#9967c7] hover:to-[#a05de2] hover:cursor-pointer px-6 py-4 h-auto">
                     Get Started <ArrowRight className="ml-2 h-5 w-5">
                     </ArrowRight>
                   </Button>
                 </Link>
                 <Link href="/about">
-                  <Button className="border bg-white text-primary text-base font-medium hover:bg-purple-50 px-6 py-4 h-auto">
+                  <Button className="border bg-transparent text-primary text-base font-medium hover:bg-purple-50 px-6 py-4 h-auto">
                     Learn More
                   </Button>
                 </Link>
               </div>
+              <p className="text-sm font-light text-muted-foreground -mt-2">Try our service with no cost.</p>
             </div>
-            <div className="relative h-[800px] lg:h-[400px] rounded-xl overflow-hidden">
-              <Image src="/img/Readibly-Hero.png" alt="hero" layout="fill" objectFit="cover" priority>              
-              </Image>
+            <div className="relative w-full h-[700px] lg:h-[500px] rounded-xl overflow-hidden">
+              <Image 
+                src="/img/Readibly-Hero.png" 
+                alt="hero" 
+                fill
+                className="object-contain"
+                priority
+              />              
             </div>
           </div>
         </div>
       </section>
 
       {/* Feature Section */}
-      <section className="w-full h-min-screen flex flex-col items-center py-12 md:py-24 bg-[#e9ecef]">
+      <section className="w-full h-min-screen flex flex-col items-center py-12 md:py-24 bg-gradient-to-br from-[#2e31ce] via-[#772abe] to-[#a8a9f3]">
         <div className="container px-4 md:px-6">
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
             <div className="space-y-4">
-              <h2 className="text-3xl font-medium sm:text-4xl md:text-5xl text-gray-800">
+              <h2 className="text-3xl font-medium sm:text-4xl md:text-5xl text-white">
                 Main Features
               </h2>
-              <p className="max-w-[700px] text-muted-foreground font-normal text-xl gap-8">
+              <p className="max-w-[700px] text-muted-foreground font-light text-xl gap-8 text-white">
                 Technology that helps people with dyslexia read more easily and comfortably.
               </p>
             </div>
           </div>
           <div className="mx-auto grid max-w-5xl items-center gap-6 py-12 lg:grid-cols-3">
             <FeatureCard
-              is3D
-              icon={<Eye className="h-8 w-8 text-[#7678ed]" />}
+              isHover
+              icon={<Eye className="h-8 w-8 text-[#2e31ce]" />}
               title="Eye-Tracking"
               description={
                 <>
                   <span className="text-blue-400">Technology</span>
                   <span> that tracks</span>
                   <span className="text-blue-400"> eye</span>
-                  <span> movements to adjust reading speed.</span>
+                  <span> movements to adjust reading speed and improve reading experience.</span>
                 </>
               }
             />
             <FeatureCard
-              is3D
-              icon={<Headphones className="h-8 w-8 text-[#7678ed]" />}
+              isHover
+              icon={<Headphones className="h-8 w-8 text-[#2e31ce]" />}
               title="Text-to-Speech"
               description={
                 <>
@@ -83,8 +90,8 @@ export default function Home() {
               }
             />
             <FeatureCard
-              is3D
-              icon={<BookOpen className="h-8 w-8 text-[#7678ed]" />}
+              isHover
+              icon={<BookOpen className="h-8 w-8 text-[#2e31ce]" />}
               title="Dyslexia Friendly"
               description={
                 <>
@@ -98,6 +105,14 @@ export default function Home() {
             />
           </div>
         </div>
+      </section>
+      
+      <section className="w-full h-min-screen flex flex-col items-center py-12 md:py-24 bg-[#FDFCF4]">
+            <div className="container px-4 md:px-6">
+              <div className="flex flex-col items-center justify-center space-y-4 text-center">
+
+              </div>
+            </div>
       </section>
     </div>
   );
